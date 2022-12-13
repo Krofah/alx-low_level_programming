17d0d4eb9ce1e6f2c8e74b0991e910af8a7e8d54
Krofah <amponsahkrofah@gmail.com>
2022-12-13 14:06:26 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of JB, start from 00:00 to 23:59
- *
+ * jack bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0);
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
