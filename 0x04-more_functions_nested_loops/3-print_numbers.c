bfad07a716680d55ab94670a4c4c3c11e9b98f06
Krofah <amponsahkrofah@gmail.com>
2022-12-15 16:09:20 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0-9
-* owned by Krofah
+*print_numbers - print num from 0-9
 *Return: void
 */
 
@@ -12,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
