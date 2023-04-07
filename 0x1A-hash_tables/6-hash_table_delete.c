4aac09112d22c2ff1eea00a3a257dc973be439e1
Krofah <krofahamponsah@gmail.com>
2023-04-07 15:42:35 -0700

    task 4

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,32 +3,49 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_delete - Deletes a hash table
-* @ht: The hash table to frees
-*
-* Return: Nothing!
-*/
+  * hash_table_delete - Deletes a hash table
+  * @ht: The hash table to frees
+  *
+  * Return: Nothing!
+  */
 void hash_table_delete(hash_table_t *ht)
 {
-if (ht == NULL)
-{
-return;
-}
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *temp = NULL;
 
-for (size_t i = 0; i < ht->size; i++)
-{
-hash_node_t *node = ht->array[i];
-while (node != NULL)
-{
-hash_node_t *temp = node;
-node = node->next;
-free(temp->key);
-free(temp->value);
-free(temp);
-}
-}
+	if (ht && ht->size && ht->array)
+	{
+		for (; i < ht->size; ++i)
+		{
+			node = ht->array[i];
 
-free(ht->array);
-free(ht);
-}
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						temp = node;
+						node = node->next;
+						free(temp->key);
+						free(temp->value);
+						free(temp);
+					}
+				}
 
+				node = ht->array[i];
+				if (node->key && node->value)
+				{
+					free(node->key);
+					free(node->value);
+				}
+			}
+
+			free(node);
+		}
+
+		free(ht->array);
+		free(ht);
+	}
+}
